4da6e338fd51056f36abb89e15a7e1a7f2f2d355
Matthew Gregg <m.gregg@synthace.com>
2023-11-14 19:46:40 +0000

    Pure script getting started example.

---
+++ my-project/src/Main.purs
@@ -0,0 +1,11 @@
+module Main where
+
+import Prelude
+
+import Euler (answer)
+import Effect (Effect)
+import Effect.Console (log)
+
+main :: Effect Unit
+main = do
+  log ("The answer is " <> show answer)
